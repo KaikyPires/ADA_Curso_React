335fb58f496c3699380e89fa250846004467aaf9
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-07 13:42:07 -0300

    feat: Add initial TypeScript files with basic functions and HTML structure

---
+++ TypeScript/index.js
@@ -0,0 +1,4 @@
+function soma(num1, num2) {
+    return console.log(num1 + num2);
+}
+soma(9, 12);
---
+++ TypeScript/index.ts
@@ -0,0 +1,12 @@
+function soma(num1: number, num2: number){
+    return console.log(num1 + num2)
+}
+soma(9,12)
+
+let numero: number = 20
+
+const pi = 3.14
+
+let correta: boolean
+
+
