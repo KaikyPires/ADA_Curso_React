7a74317d688328ee3da5f642ad0d64f9f4a99f3e
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-09 12:09:44 -0300

    feat: Add initial HTML structure, CSS styles, and JavaScript for DOM manipulation

---
+++ Browser Api/js/index.js
@@ -0,0 +1,36 @@
+// Selecionar elementos HTML
+
+// 1.Pelo nome da tag
+const todosH1 = document.getElementsByTagName('h1')
+console.log(todosH1)
+
+// 2. Pelo nome da classe
+const classParagrafo = document.getElementsByClassName('paragrafo')
+console.log(classParagrafo)
+
+// 3. Pegar pelo name
+const pegarPeloName = document.getElementsByName('email')
+console.log(pegarPeloName)
+
+// 4. Pelo ID
+const pegarPeloId = document.getElementById('image')
+console.log(pegarPeloId)
+
+// 5. Query Selector
+const imagem = document.querySelector('body > img')
+console.log(imagem)
+
+// 6. Query Selector ALL
+const p = document.querySelector('p')
+console.log(p)
+
+console.clear()
+
+// Acessando/ alterando conteudo das tags
+const primeiroParagrafo = document.querySelector('p.paragrafo')
+console.log('textContent: ', primeiroParagrafo.textContent) // Só texto
+console.log('innerHtml: ', primeiroParagrafo.innerHTML) //Pega as tags
+
+primeiroParagrafo.textContent = 'Outra coisa'
+primeiroParagrafo.innerHTML = 'Outra <strong>coisa<strong/>'
+
