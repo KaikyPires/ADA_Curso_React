ec3cb8f0104240a91ba07c9d05ae3e7ac112c5f6
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-10 20:06:48 -0300

    feat: Enhance event counter functionality and add styling for buttons

--- Browser apI II/js/index.js
+++ Browser apI II/js/index.js
@@ -6,17 +6,25 @@ const contador = document.querySelector('#contador')
 const input = document.querySelector('input')
 aumentarButton.addEventListener('click', (event) =>{
     const valorAtual = Number(contador.textContent)
-
+    
     contador.textContent = String(valorAtual + 1)
+
+    aumentarButton.classList.add('btn')
+    diminuirButton.classList.remove('btn')
 } )
 
 diminuirButton.addEventListener('click', ()=> {
     const valorAtual = Number(contador.textContent)
+    aumentarButton.classList.remove('btn')
+    diminuirButton.classList.add('btn')
+    
     if(valorAtual != 0){
         contador.textContent = String(valorAtual -1)
     }
     else{
+        contador.style.color = 'red'
         alert('Valor zerado')
+       
     }
     
 })
@@ -24,3 +32,9 @@ diminuirButton.addEventListener('click', ()=> {
 input.addEventListener('input', () =>{
     console.log(input.value)
 })
+
+// Alterando estilo
+contador.style.padding = '0 2rem'
+contador.style.border = 'solid 2px'
+
+
