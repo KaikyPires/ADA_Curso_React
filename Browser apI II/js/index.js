33677de70a92ad5069bf8cf25b672114e906af0f
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-11 17:33:15 -0300

    feat: Implement dark mode toggle functionality and improve button styling

--- Browser apI II/js/index.js
+++ Browser apI II/js/index.js
@@ -1,38 +1,46 @@
-const aumentarButton = document.querySelector('#aumentar')
-const diminuirButton = document.querySelector('#diminuir')
+const aumentarButton = document.querySelector("#aumentar");
+const diminuirButton = document.querySelector("#diminuir");
 
-const contador = document.querySelector('#contador')
+const contador = document.querySelector("#contador");
 
-const input = document.querySelector('input')
-aumentarButton.addEventListener('click', (event) =>{
-    const valorAtual = Number(contador.textContent)
-    
-    contador.textContent = String(valorAtual + 1)
+const input = document.querySelector("input");
+aumentarButton.addEventListener("click", (event) => {
+  const valorAtual = Number(contador.textContent);
 
-    aumentarButton.classList.add('btn')
-    diminuirButton.classList.remove('btn')
-} )
+  contador.textContent = String(valorAtual + 1);
 
-diminuirButton.addEventListener('click', ()=> {
-    const valorAtual = Number(contador.textContent)
-    aumentarButton.classList.remove('btn')
-    diminuirButton.classList.add('btn')
-    
-    if(valorAtual != 0){
-        contador.textContent = String(valorAtual -1)
-    }
-    else{
-        contador.style.color = 'red'
-        alert('Valor zerado')
-    }
-})
+  aumentarButton.classList.add("btn");
+  diminuirButton.classList.remove("btn");
+});
 
-input.addEventListener('input', () =>{
-    console.log(input.value)
-})
+diminuirButton.addEventListener("click", () => {
+  const valorAtual = Number(contador.textContent);
+  aumentarButton.classList.remove("btn");
+  diminuirButton.classList.add("btn");
+
+  if (valorAtual != 0) {
+    contador.textContent = String(valorAtual - 1);
+  } else {
+    contador.style.color = "red";
+    alert("Valor zerado");
+  }
+});
+
+input.addEventListener("input", () => {
+  console.log(input.value);
+});
 
 // Alterando estilo
-contador.style.padding = '0 2rem'
-contador.style.border = 'solid 2px'
+contador.style.padding = "0 2rem";
+contador.style.border = "solid 2px";
 
+const body = document.querySelector("body");
 
+const mode = document.querySelector("#mode");
+
+const h1 = document.querySelector("h1");
+
+mode.addEventListener("click", () => {
+  body.classList.toggle("body-darkMode");
+  h1.classList.toggle("h1-darkMode");
+});
