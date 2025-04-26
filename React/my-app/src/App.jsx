1003d82b537ec5a55ea52032f94de45e4267ba49
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-26 12:57:45 -0300

    feat: Add Article component and associated styles for displaying articles

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+import { Article } from "./components/Articles/Article";
 
-
-import Navbar from "./components/Navbar/Navbar"
+import googleImage from "./assets/images/google.png";
+import finacialImage from "./assets/images/financial.png";
+import dashboardImage from "./assets/images/dashboard.png";
+import Navbar from "./components/Navbar/Navbar";
+import { Counter } from "./components/Counter/Counter";
 // Componente em classe é uma classe que herda a classe Component do React, e retorna o html dentro do metodo render
 class App extends React.Component {
-  render(){
-    return(<Navbar/>);
+  render() {
+    return (
+      <>
+        <Navbar />
+        {/* <section id="articles">
+          <Article
+            title="Dashboard"
+            provider="NASA"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione neque enim rem amet quasi fugiat maiores, 
+    quos reprehenderit ipsum beatae dolorum laboriosam magnam ipsam, explicabo atque cupiditate! Repellendus, magnam ullam."
+            thumb={dashboardImage}
+          />
+
+          <Article
+            title="Analytics Overview"
+            provider="Google"
+            description="Discover insights from data trends and user behavior, helping your business make smarter, data-driven decisions every day."
+            thumb={googleImage}
+          />
+
+          <Article
+            title="Financial Report"
+            provider="Bloomberg"
+            description="Get the latest updates on market movements, economic indicators, and expert analysis to guide your investment strategy."
+            thumb={finacialImage}
+          />
+        </section> */}
+        <Counter />
+      </>
+    );
   }
 }
 
--- React/my-app/src/components/Articles/Article.jsx
+++ React/my-app/src/components/Articles/Article.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-
+import nasaImage from '../../assets/images/dashboard.png'
+import './style.css'
 export class Article extends React.Component{
-    
+    render(){
+        return(
+            <article id="article">
+                <img src={this.props.thumb} alt="" />
+                <div className="article-infos">
+                    <h2>{this.props.title}</h2>
+                    <h3>{this.props.provider}</h3>
+                    <p>{this.props.description}</p>
+                </div>
+            </article>
+        );
+    }
 }
