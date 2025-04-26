4b6ef9068fa92cd993b461179b8a0668679ebf1c
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-25 21:04:42 -0300

    feat: Add App component and logo image to the project

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+import Navbar from "./components/Navbar"
+// Componente em classe é uma classe que herda a classe Component do React, e retorna o html dentro do metodo render
+class App extends React.Component {
+  render(){
+    return(<Navbar/>);
+  }
+}
+
+export default App;
