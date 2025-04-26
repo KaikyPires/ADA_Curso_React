52565fa2c54f0c1bb73371a715daae2ef77c62fa
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-26 10:07:11 -0300

    refactor: Update Navbar import path and remove unused Navbar component

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar/Navbar"
 // Componente em classe é uma classe que herda a classe Component do React, e retorna o html dentro do metodo render
 class App extends React.Component {
   render(){
