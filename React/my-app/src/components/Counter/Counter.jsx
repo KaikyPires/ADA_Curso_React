4196f393a37538b252aa437c1dc248c75910a06d
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-05-02 14:03:28 -0300

    refactor: Update Counter component to use state for managing contador

--- React/my-app/src/components/Counter/Counter.jsx
+++ React/my-app/src/components/Counter/Counter.jsx
@@ -4,15 +4,16 @@ import React from "react"
 export class Counter extends React.Component{
     constructor(){
         super()
-        this.contador = 0;
+        // this.contador = 0;
+        this.state = { contador: 0}
     }
     render(){
         return(
             <div style={{marginTop:"20px", marginLeft:"20px", marginRight:"20px"}}>
-                <h1 >{this.contador}</h1>
+                <h1 >{this.state.contador}</h1>
             <div>
-            <button onClick={() => {this.contador-=1}}>diminuir</button>
-            <button onClick={() => {this.contador +=1}}>aumentar</button>
+            <button onClick={() => { this.setState({ contador: this.state.contador - 1 }) }}> diminuir</button>
+            <button onClick={() => {this.setState ({contador: this.state.contador + 1})}}>aumentar</button>
             </div>
             </div>
     
